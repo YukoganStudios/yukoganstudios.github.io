f23ae69401532793ea830175ac4af90fceff0c4b
Yukogan <73695279+YukoganStudios@users.noreply.github.com>
2021-01-15 14:27:11 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function reverse(str) {
+    array = Array.from(str);
+    //console.log(array);
+
+    var result = '';
+    for (i = array.length - 1;i >= 0; i --) {
+        //console.log(array[i]);
+        //document.getElementById("str").innerHTML = document.getElementById("str").innerHTML + array[i]
+        //console.log(ar6 + ar5 + ar4 + ar3 + ar2 + ar1)
+        result = result + array[i];
+    }
+    return result;
+}
