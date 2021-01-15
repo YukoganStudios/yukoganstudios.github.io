87bda56da6c7661d9af10328ddd20ef5bfc30772
Yukogan <73695279+YukoganStudios@users.noreply.github.com>
2021-01-15 14:39:55 +0100

    Update main.js

--- main.js
+++ main.js
@@ -5,10 +5,10 @@ function reverse(str) {
     var result = '';
     for (i = array.length - 1;i >= 0; i --) {
         //console.log(array[i]);
-        //document.getElementById("str").innerHTML = document.getElementById("str").innerHTML + array[i]
+        document.getElementById("str").innerHTML = 'yaguyeh';
         //console.log(ar6 + ar5 + ar4 + ar3 + ar2 + ar1)
         //result = result + array[i];
-        result = 'yaguyeh';
+        //result = 'yaguyeh';
     }
     return result;
 }
